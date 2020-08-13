ccf72b8ac6ea55653d3f06e476bf3de630a49b8c
MANEESH P <mputhenhouse8@gmail.com>
2020-08-13 19:14:44 +0530

    style changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "../src/components/footer/Footer";
 const Home = React.lazy(() => import("../src/components/home/Home"));
-const About = React.lazy(() => import("../src/components/home/About"));
-const Works = React.lazy(() => import("../src/components/home/Works"));
-const Art = React.lazy(() => import("../src/components/home/Art"));
-const Navbar = React.lazy(() => import("../src/components/home/Navbar"));
-const Footer = React.lazy(() => import("../src/components/home/Footer"));
+const About = React.lazy(() => import("../src/components/about/About"));
+const Works = React.lazy(() => import("../src/components/works/Works"));
+const Art = React.lazy(() => import("../src/components/art/Art"));
+const Navbar = React.lazy(() => import("../src/components/navbar/Navbar"));
 
-// import Home from "../src/components/home/Home";
-// import About from "../src/components/about/About";
-// import Works from "../src/components/works/Works";
-// import Art from "../src/components/art/Art";
-// import Navbar from "../src/components/navbar/Navbar";
 function App() {
   return (
     <div className="App">
@@ -19,10 +14,18 @@ function App() {
         <Suspense fallback={Footer}>
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/code" component={Works} />
-            <Route exact path="/art" component={Art} />
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/code">
+              <Works />
+            </Route>
+            <Route exact path="/art">
+              <Art />
+            </Route>
           </Switch>
         </Suspense>
       </Router>
