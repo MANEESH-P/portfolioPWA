19b65bbc7f73589555783b4626d365ac001ed603
MANEESH P <mputhenhouse8@gmail.com>
2020-08-14 14:31:49 +0530

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "../src/components/footer/Footer";
-import Home from "../src/components/home/Home";
-import About from "../src/components/about/About";
-import Works from "../src/components/works/Works";
-import Art from "../src/components/art/Art";
-import Navbar from "../src/components/navbar/Navbar";
+// import Home from "../src/components/home/Home";
+// import About from "../src/components/about/About";
+// import Works from "../src/components/works/Works";
+// import Art from "../src/components/art/Art";
+// import Navbar from "../src/components/navbar/Navbar";
 
-// const Home = React.lazy(() => import("../src/components/home/Home"));
-// const About = React.lazy(() => import("../src/components/about/About"));
-// const Works = React.lazy(() => import("../src/components/works/Works"));
-// const Art = React.lazy(() => import("../src/components/art/Art"));
-// const Navbar = React.lazy(() => import("../src/components/navbar/Navbar"));
+const Home = React.lazy(() => import("../src/components/home/Home"));
+const About = React.lazy(() => import("../src/components/about/About"));
+const Works = React.lazy(() => import("../src/components/works/Works"));
+const Art = React.lazy(() => import("../src/components/art/Art"));
+const Navbar = React.lazy(() => import("../src/components/navbar/Navbar"));
 
 function App() {
   return (
     <div className="App">
-      {/* <Suspense
+      <Suspense
         fallback={
           <div
             style={{
@@ -28,25 +28,25 @@ function App() {
             <Footer />
           </div>
         }
-      > */}
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/code">
-            <Works />
-          </Route>
-          <Route exact path="/art">
-            <Art />
-          </Route>
-        </Switch>
-      </Router>
-      {/* </Suspense> */}
+      >
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/code">
+              <Works />
+            </Route>
+            <Route exact path="/art">
+              <Art />
+            </Route>
+          </Switch>
+        </Router>
+      </Suspense>
     </div>
   );
 }
